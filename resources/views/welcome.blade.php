f7bdf738c284ba20286ac6ad8311afb6a0828704
Nick <webersoftware.dev@gmail.com>
2021-10-06 08:34:14 -0400

    fixed welcome page div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,29 +3,16 @@
 
 @section('content')
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="content">
-        <div class="title m-b-md">
-                Sub Creator
+            <div class="title m-b-md">
+                    Sub Creator
+                </div>
+                <img class="main-image" src="/img/sub.jpg" alt="sub picture">
+                <h1> Just For Fun</h1>
+                <p class="mssg"> {{ session('mssg') }} </p>
+                <a class="create" href="/subs/create">Create a Sub</a>
+                <a class="create" href="/subs">Saved Subs</a>
             </div>
-            <img class="main-image" src="/img/sub.jpg" alt="sub picture">
-            <h1> Just For Fun</h1>
-            <p class="mssg"> {{ session('mssg') }} </p>
-            <a class="create" href="/subs/create">Create a Sub</a>
-            <a class="create" href="/subs">Saved Subs</a>
         </div>
     </div>
 @endsection
